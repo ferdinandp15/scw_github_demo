361c75f19dd66c0c3a5be41a837b0964b3274fd7
ferdinandp15 <ferdinand.perez@gmail.com>
2023-06-05 13:06:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/foo/:id', (req, res) => {
-    res.send('Hello world!')
+eval('console.log("something", ' + req.params.id + ')')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
